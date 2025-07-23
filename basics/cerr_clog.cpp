4859c1e7e19ef5db2e1d823a73f1e7814f190f58
Sheikh Aalam Kawser <sheikhaalam068@gmail.com>
2025-07-23 12:58:35 +0530

    Delete basics directory
    added new one after formatting

--- basics/cerr_clog.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main(){
-std::cerr << "Not found!" << std::endl;
-std::clog << "This is a log message!" << std::endl;
-    return 0;
-}
--- basics/sum.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-
-int main(){
- int a=2, b=3;
-
- std::cout << "The sum is: " << a+b << std::endl;
-}
